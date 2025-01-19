c5a50c343c8bb6f726c9189bf07e4cf2ddd27a06
Aarushi2146 <aarug2146@gmail.com>
2025-01-20 03:32:50 +0530

    Numbers to words code

---
+++ NumtoWord.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class NumtoWord{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number from 1 to 10");
+        int n = sc.nextInt();
+        if(n==1){
+            System.out.println("One");
+        }
+        if(n==2){
+            System.out.println("Two");
+        }
+        if(n==3){
+            System.out.println("Three");
+        }
+        if(n==4){
+            System.out.println("Four");
+        }
+        if(n==5){
+            System.out.println("Five");
+        }
+        if(n==6){
+            System.out.println("Six");
+        }
+        if(n==7){
+            System.out.println("Seven");
+        }
+        if(n==8){
+            System.out.println("Eight");
+        }
+        if(n==9){
+            System.out.println("Nine");
+        }
+        if(n==10){
+            System.out.println("Ten");
+        }
+    }
+}
