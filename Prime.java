d4f6e442d1d560fdd9a2ac5fa98a2a66644bc9f3
Aarushi2146 <aarug2146@gmail.com>
2025-01-20 03:44:17 +0530

    prime code

---
+++ Prime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Prime{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the start of the range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the end of the range: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
